0a7c4136680f22f1633b376b61f39f0e62f812d8
KARINE2008 <karinefmiguel1@gmail.com>
2025-06-24 16:33:07 -0400

    01

--- _parts/_footer.php
+++ _parts/_footer.php
@@ -8,10 +8,10 @@
         <p><i class="bi bi-person-lines-fill"></i>(69)12345-6789</p>
 
     </div>
-    <div class="r-linha-item">
-        <a href="#"><i class="bi bi-facebook"></i></a>
-        <a href="#"><i class="bi bi-whatsapp"></i></a>
-        <a href="#"><i class="bi bi-instagram"></i></a>
+    <div class="r-linha-item rede-social">
+        <a href="https://www.facebook.com/checkpoint/1501092823525282/?next=https%3A%2F%2Fwww.facebook.com%2F%3Flsrc%3Dlb#"><i class="bi bi-facebook"></i></a>
+        <a href="https://wa.me/5569984374910" target="_blank"><i class="bi bi-whatsapp"></i></a>
+        <a href="https://www.instagram.com/karine_favaleca/" target="_blank"><i class="bi bi-instagram"></i></a>
     </div>
 </div>
 <p>&copy; 2025 Todos o direitos reservados</p>
