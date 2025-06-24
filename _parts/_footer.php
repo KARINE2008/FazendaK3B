2a72cb5a0a3795f2ceedadab96efcb770e2c4e45
KARINE2008 <karinefmiguel1@gmail.com>
2025-06-24 16:05:35 -0400

    0

--- _parts/_footer.php
+++ _parts/_footer.php
@@ -12,12 +12,6 @@
         <a href="#"><i class="bi bi-facebook"></i></a>
         <a href="#"><i class="bi bi-whatsapp"></i></a>
         <a href="#"><i class="bi bi-instagram"></i></a>
-        <a href="#"><i class="bi bi-twitter"></i></a>
-        <a href="#"><i class="bi bi-linkedin"></i></a>
-        <a href="#"><i class="bi bi-envelope-at"></i></a>
-        <a href="#"><i class="bi bi-tiktok"></i></a>
-        <a href="#"><i class="bi bi-spotify"></i></a>
-        <a href="#"><i class="bi bi-pinterest"></i></a>
     </div>
 </div>
 <p>&copy; 2025 Todos o direitos reservados</p>
